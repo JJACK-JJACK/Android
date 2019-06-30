c646b7298bc4b8d31d541185e2dd9fd9a303f4b1
S-Hayoung <37989578+gds04070@users.noreply.github.com>
2019-06-30 15:51:33 +0900

    init

---
+++ app/src/androidTest/java/jjackjjack/sopt/com/jjackjjack/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package jjackjjack.sopt.com.jjackjjack
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("jjackjjack.sopt.com.jjackjjack", appContext.packageName)
+    }
+}
