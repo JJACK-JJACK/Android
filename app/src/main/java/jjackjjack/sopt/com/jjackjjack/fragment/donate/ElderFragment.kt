ce6fee87b7c072a34972ed87b7b755671bb6d409
Yooon-hee2 <yoooni.kim2@gmail.com>
2019-07-03 01:51:43 +0900

    add fragment & modify donate views

--- app/src/main/java/jjackjjack/sopt/com/jjackjjack/fragment/donate/ElderFragment.kt
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/fragment/donate/ElderFragment.kt
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import jjackjjack.sopt.com.jjackjjack.DonateSortCategoryPagerAdapter
 import jjackjjack.sopt.com.jjackjjack.R
+import kotlinx.android.synthetic.main.fragment_elder_category.*
 
 class ElderFragment : Fragment(){
 
@@ -13,6 +16,27 @@ class ElderFragment : Fragment(){
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_elder_category, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        var sub_adapter = DonateSortCategoryPagerAdapter(activity!!.supportFragmentManager)
+        donate_sort_pager.adapter = sub_adapter
+
+        donate_sort_tab.setupWithViewPager(donate_sort_pager)
+
+        val navDonateListSort: View =
+            (activity!!.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                .inflate(R.layout.nav_donatelist_sort, null, false)
+
+        donate_sort_tab.getTabAt(0)!!.customView =
+            navDonateListSort.findViewById(R.id.nav_donatelist_sort_recent) as RelativeLayout
+        donate_sort_tab.getTabAt(1)!!.customView =
+            navDonateListSort.findViewById(R.id.nav_donatelist_sort_popular) as RelativeLayout
+        donate_sort_tab.getTabAt(2)!!.customView =
+            navDonateListSort.findViewById(R.id.nav_donatelist_sort_unpopular) as RelativeLayout
+
     }
 
 }
---
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/fragment/donate/PopularFragment.kt
@@ -0,0 +1,17 @@
+package jjackjjack.sopt.com.jjackjjack.fragment.donate
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jjackjjack.sopt.com.jjackjjack.R
+
+class PopularFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_popular_category, container, false)
+
+    }
+}
