d3e29f1623011dca6d4b2eadbf6ae6abf6ed9cb1
S-Hayoung <37989578+gds04070@users.noreply.github.com>
2019-07-04 02:52:42 +0900

    2019/07/04 drawer menu

--- app/src/main/java/jjackjjack/sopt/com/jjackjjack/MainActivity.kt
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.view.Gravity
 import android.view.MenuItem
+import com.example.kmj.imageslider.RankActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_activity_main.*
 import kotlinx.android.synthetic.main.nav_drawer.*
@@ -53,6 +54,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             if(ly_drawer.isDrawerOpen(Gravity.END)){
                 ly_drawer.closeDrawer(Gravity.END)
             }
+        }
+
+        btn_drawer_donate_record.setOnClickListener {
+            startActivity<DonateRecordActivity>()
+            ly_drawer.closeDrawer(Gravity.END)
+            //finish()
+        }
+
+        btn_drawer_rank.setOnClickListener {
+            startActivity<RankActivity>()
+            ly_drawer.closeDrawer(Gravity.END)
+            //finish()
+        }
+
+        btn_drawer_mypage.setOnClickListener {
+            startActivity<MyPageActivity>()
+            ly_drawer.closeDrawer(Gravity.END)
+           // finish()
+        }
+
+        btn_drawer_berrycharge.setOnClickListener {
+            startActivity<BerryChargeActivity>()
+            ly_drawer.closeDrawer(Gravity.END)
+           // finish()
         }
 
     }
