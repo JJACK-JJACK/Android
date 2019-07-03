e37469b61d77fe9d50d91c2ea61f5ca043affbfa
S-Hayoung <37989578+gds04070@users.noreply.github.com>
2019-07-04 04:53:09 +0900

    2019/07/04 drawer + activity

--- app/src/main/java/jjackjjack/sopt/com/jjackjjack/MainActivity.kt
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/MainActivity.kt
@@ -8,10 +8,12 @@ import android.view.MenuItem
 import com.example.kmj.imageslider.RankActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_activity_main.*
+import kotlinx.android.synthetic.main.content_activity_main.btn_hambuger
 import kotlinx.android.synthetic.main.nav_drawer.*
+import kotlinx.android.synthetic.main.toolbar_with_hamburger.*
 import org.jetbrains.anko.startActivity
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +21,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         initalUI()
     }
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
 
     private fun initalUI(){
-        btn_hambuger.setOnClickListener {
-            if(!ly_drawer.isDrawerOpen(Gravity.END)){
-                ly_drawer.openDrawer(Gravity.END)
-            }
-        }
 
         drawerUI()
 
@@ -44,6 +39,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun drawerUI(){
+        btn_hambuger.setOnClickListener {
+            if(!ly_drawer.isDrawerOpen(Gravity.END)){
+                ly_drawer.openDrawer(Gravity.END)
+            }
+        }
+        btn_drawer_donate_record.setOnClickListener {
+            startActivity<DonateRecordActivity>()
+            ly_drawer.closeDrawer(Gravity.END)
+            //finish()
+        }
         btn_drawer_home.setOnClickListener {
             startActivity<MainActivity>()
             ly_drawer.closeDrawer(Gravity.END)
@@ -56,11 +61,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         }
 
-        btn_drawer_donate_record.setOnClickListener {
-            startActivity<DonateRecordActivity>()
-            ly_drawer.closeDrawer(Gravity.END)
-            //finish()
-        }
 
         btn_drawer_rank.setOnClickListener {
             startActivity<RankActivity>()
@@ -71,13 +71,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         btn_drawer_mypage.setOnClickListener {
             startActivity<MyPageActivity>()
             ly_drawer.closeDrawer(Gravity.END)
-           // finish()
+            // finish()
         }
 
         btn_drawer_berrycharge.setOnClickListener {
             startActivity<BerryChargeActivity>()
             ly_drawer.closeDrawer(Gravity.END)
-           // finish()
+            // finish()
         }
 
     }
