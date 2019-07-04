3ee02b22d7c3d28114acde2415fe9d2f92793a70
S-Hayoung <37989578+gds04070@users.noreply.github.com>
2019-07-05 04:53:05 +0900

    2019/07/05 Network initial work

---
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/network/ApplicationController.kt
@@ -0,0 +1,28 @@
+package jjackjjack.sopt.com.jjackjjack.network
+
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApplicationController : Application(){
+    private val baseURL = ""
+//주소 뜨기전까지 주석 살리지 말것
+    lateinit var networkService: NetworkService
+
+    companion object{
+        lateinit var instance: ApplicationController
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+       // buildNetWork()
+    }
+/*
+    private fun buildNetWork(){
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl(baseURL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        networkService = retrofit.create(NetworkService::class.java)
+    }*/
+}
