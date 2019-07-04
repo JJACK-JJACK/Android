cfd65ba2114b0d5f5e263a177cf7507f367598fd
S-Hayoung <37989578+gds04070@users.noreply.github.com>
2019-07-05 01:34:46 +0900

    2019/07/05 DepositActivity Code arrangement

--- app/src/main/java/jjackjjack/sopt/com/jjackjjack/BerryChargeActivity.kt
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/BerryChargeActivity.kt
@@ -2,11 +2,20 @@ package jjackjjack.sopt.com.jjackjjack
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_berry_charge.*
+import org.jetbrains.anko.startActivity
 
 class BerryChargeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_berry_charge)
+        initialUI()
+    }
+
+    private fun initialUI(){
+        btn_berry_charge_npb.setOnClickListener {
+            startActivity<DepositActivity>()
+        }
     }
 }
