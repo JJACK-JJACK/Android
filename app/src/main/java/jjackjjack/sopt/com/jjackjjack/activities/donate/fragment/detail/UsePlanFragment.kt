4f6a7e7bff761f965cc802ca43eaf12bb28c0362
Yooon-hee2 <yoooni.kim2@gmail.com>
2019-07-08 06:07:57 +0900

    modify ui

--- app/src/main/java/jjackjjack/sopt/com/jjackjjack/activities/donate/fragment/detail/UsePlanFragment.kt
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/activities/donate/fragment/detail/UsePlanFragment.kt
@@ -18,7 +18,7 @@ class UsePlanFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_use_berry, container, false)
+        return inflater.inflate(R.layout.fragment_use_plan, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
