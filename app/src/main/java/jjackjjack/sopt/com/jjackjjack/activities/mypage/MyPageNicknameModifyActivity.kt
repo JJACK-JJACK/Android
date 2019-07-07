e4e3a0cdfd9e25bf887793d98c199def3bd61779
Yooon-hee2 <yoooni.kim2@gmail.com>
2019-07-08 01:58:36 +0900

    extract strings and modify mypage activity

---
+++ app/src/main/java/jjackjjack/sopt/com/jjackjjack/activities/mypage/MyPageNicknameModifyActivity.kt
@@ -0,0 +1,13 @@
+package jjackjjack.sopt.com.jjackjjack.activities.mypage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jjackjjack.sopt.com.jjackjjack.R
+
+class MyPageNicknameModifyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_nickname_modify)
+    }
+}
